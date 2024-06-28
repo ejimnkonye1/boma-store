8f8d81ab27c3026e796afd26e59153c530b91b3d
boma <94929211+Bomagift@users.noreply.github.com>
2024-06-28 06:13:25 -0400

    Styled the product page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -79,12 +79,12 @@ const images = product.image || [];
       </div>{" "}
     
 
-<div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+<div className="offcanvas offcanvas-end" data-bs-scroll="true" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+  <div className="offcanvas-header">
+    <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
-  <div class="offcanvas-body">
+  <div className="offcanvas-body">
     ...
     {cartItem.map((item) => (
       <div>
